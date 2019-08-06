109533343713d9ed09af1414e13ea647833feacd
khofa prayogo <khofaprayogo26@gmail.com>
2019-08-06 11:31:01 +0700

    kelas done

--- src/main/java/backend/model/Kelas.java
+++ src/main/java/backend/model/Kelas.java
@@ -17,6 +17,21 @@ public class Kelas {
     private String kode_kelas;
     @NotNull(message = "Nama Materi harus diisi.")
     private String jenis_kelas;
+///////
+    @NotNull(message = "Jam Pilihan harus diisi.")
+    private String jam_pilihan;
+
+    @NotNull(message = "Tanggal Mulai harus diisi.")
+    private String tanggal_mulai;
+
+    @NotNull(message = "Target Peserta harus diisi.")
+    private String target_peserta;
+
+    @NotNull(message = "Status harus diisi.")
+    private String status;
+    /////////////
+
+
 
     public String getKode_materi() {
         return kode_kelas;
@@ -33,6 +48,43 @@ public class Kelas {
     public void setJenis_kelas(String jenis_kelas) {
         this.jenis_kelas = jenis_kelas;
     }
+
+
+    //////////////
+    public String getJam_pilihan() {
+      return jam_pilihan;
+  }
+
+  public void setjam_pilihan(String jam_pilihan) {
+      this.jam_pilihan = jam_pilihan;
+  }
+
+
+  public String getTanggal_mulai() {
+    return tanggal_mulai;
+}
+
+public void setTanggal_mulai(String tanggal_mulai) {
+    this.tanggal_mulai = tanggal_mulai;
+}
+
+
+public String getTarget_peserta() {
+  return target_peserta;
+}
+
+public void setTarget_peserta(String target_peserta) {
+  this.target_peserta = target_peserta;
+}
+
+public String getStatus() {
+  return status;
+}
+
+public void setStatus(String status) {
+  this.status = status;
+}
+
 
     public Long getId() {
         return id;
