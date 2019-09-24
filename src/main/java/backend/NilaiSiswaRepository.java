2885710a3cb1bf65167d1f2ab217249de5c13957
akbarlaz <akbar.lazuardi@yahoo.com>
2019-09-24 11:30:30 +0700

    add nilai siswa repo

---
+++ src/main/java/backend/NilaiSiswaRepository.java
@@ -0,0 +1,16 @@
+package backend;
+
+import backend.model.NilaiSiswa;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public interface NilaiSiswaRepository {
+    
+    NilaiSiswa save(NilaiSiswa nilaiSiswa);
+    NilaiSiswa update(Integer id, @NotNull NilaiSiswa nilaiSiswa);
+    List<NilaiSiswa> findAll();
+    NilaiSiswa findById(@NotNull Integer id);
+    void deleteById(@NotNull Integer id);
+
+}
