64bb8c039e48871aaf2cd9f4911661c083511e7d
Supi Core <supi.core@gmail.com>
2019-09-26 09:48:26 +0700

    Add response file : NilaiAkhirResponse.java

---
+++ src/main/java/backend/model/NilaiAkhirResponse.java
@@ -0,0 +1,40 @@
+package backend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author : supi.core@gmail.com
+ */
+public class NilaiAkhirResponse {
+
+    public String status;
+    public String message;
+    public ArrayList<NilaiAkhir> data;
+
+    public NilaiAkhirResponse(String status, String message, List<NilaiAkhir> nilaiAkhir) {
+        this.status = status;
+        this.message = message;
+        this.data = new ArrayList<NilaiAkhir>();
+
+        for(NilaiAkhir item:nilaiAkhir){
+            this.data.add(item);
+        }
+    }
+
+    public NilaiAkhirResponse (String status, String message, NilaiAkhir nilaiAkhir){
+        this.status = status;
+        this.message = message;
+        this.data = new ArrayList<NilaiAkhir>();
+
+        if(nilaiAkhir != null){
+            this.data.add(nilaiAkhir);
+        }
+    }
+
+    public NilaiAkhirResponse (String status, String message){
+        this.status = status;
+        this.message = message;
+    }
+
+}
