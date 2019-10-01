184a223a401f036bcc846c808df8f913766e2e2d
akbarlaz <akbar.lazuardi@yahoo.com>
2019-10-02 01:50:35 +0700

    2019-10-02 remove unused imports, add author comment, add constraints, fix relational mapping

--- src/main/java/backend/model/NilaiSiswa.java
+++ src/main/java/backend/model/NilaiSiswa.java
@@ -1,143 +1,118 @@
 package backend.model;
 
 import java.util.Date;
+import java.util.Objects;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
 
+/**
+ * Author : akbar.lazuardi@yahoo.com | akbarlaz@github.com
+ */
+
 @Entity
-@Table(name = "nilai_siswa")
+@Table(name="nilai_siswa", uniqueConstraints = @UniqueConstraint(
+        columnNames = {
+        "id_kelas_peserta",
+        "id_kategori_nilai_materi"
+    }
+))
 public class NilaiSiswa {
 
     @Id
-    @GenericGenerator(name="incrementId",strategy="increment")
-    @GeneratedValue(generator = "incrementId")
-    @Column(name = "id_nilai_siswa")
-    private Integer id_nilai_siswa;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_kelas_peserta", insertable = false, updatable = false, nullable = false)
+    private KelasPeserta kelasPeserta;
+    private Long id_kelas_peserta;
+ 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_kategori_nilai_materi", insertable = false, updatable = false, nullable = false)
+    private KategoriNilaiMateri kategoriNilaiMateri;
+    private Long id_kategori_nilai_materi;
 
-    @ManyToOne(targetEntity = UserDetail.class, optional = false)
-    @JoinColumn(name = "id_user", referencedColumnName = "id", insertable = false, updatable = false)
-    private UserDetail user;
-    @Column(nullable=false)
-    private Long id_user;
-
-    /**
-     * @return the id_user
-     */
-    public Long getId_user() {
-        return id_user;
-    }
-    /**
-     * @param id_user the id_user to set
-     */
-    public void setId_user(Long id_user) {
-        this.id_user = id_user;
-    }
-    /*
-        Relasi ke tabel materi serta getter dan setternya, sementara di non aktifkan
-        @NotNull
-        @ManyToOne(targetEntity = Materi.class)
-        @JoinColumn(name = "id_materi")
-        private Materi materi;
-        public Materi getMateri() {return materi;}
-        public void setMateri(Materi materi) {this.materi = materi;} 
-    */
-    
-
-    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = KategoriNilai.class, optional = false)
-    @JoinColumn(name = "id_kategori_nilai", referencedColumnName = "id_kategori_nilai", updatable = false, insertable = false, nullable = true)
-    private KategoriNilai kategoriNilai; 
-
-    @Column(nullable=false)
-    private Integer id_kategori_nilai;
-
-    /**
-     * @return the id_kategori_nilai
-     */
-    public Integer getId_kategori_nilai() {
-        return id_kategori_nilai;
-    }
-    /**
-     * @param id_kategori_nilai the id_kategori_nilai to set
-     */
-    public void setId_kategori_nilai(Integer id_kategori_nilai) {
-        this.id_kategori_nilai = id_kategori_nilai;
-    }
-
-    @NotNull
-    @Column(name = "nilai_input")
     private Float nilai_input;
-
-    @NotNull
-    @Column(name = "nilai_hitung")
     private Float nilai_hitung;
-
-    @NotNull
-    @Column(name = "created_by")
+    @NotNull(message = "created_by tidak boleh kosong")
     private Long created_by;
-
-    @Column(name = "created_date", nullable = false, updatable = false)
     @CreationTimestamp
     private Date created_date;
-    
-    @Column(name = "updated_by", nullable = false)
+    @NotNull(message = "updated_by tidak boleh kosong")
     private Long updated_by;
-
-    @Column(name = "updated_date", nullable = false, updatable = true)
     @UpdateTimestamp
     private Date updated_date;
 
     /**
-     * @return the id_nilai_siswa
+     * @return the id
      */
-    public Integer getId_nilai_siswa() {
-        return id_nilai_siswa;
+    public Long getId() {
+        return id;
     }
     /**
-     * @param id_nilai_siswa the id_nilai_siswa to set
+     * @param id the id to set
      */
-    public void setId_nilai_siswa(Integer id_nilai_siswa) {
-        this.id_nilai_siswa = id_nilai_siswa;
-    }
-
-    public UserDetail getUser() {
-        return user;
-    }
-    public void setUser(UserDetail user) {
-        this.user = user;
-    }
-
-    /* public KategoriNilai getKategoriNilai() {
-        return kategoriNilai;
-    }
-    public void setKategoriNilai(KategoriNilai kategoriNilai) {
-        this.kategoriNilai = kategoriNilai;
-    } */
-
-    /**
-     * @return the nilai_input
-     */
-    public Float getNilai_input() {
-        return nilai_input;
+    public void setId(Long id) {
+        this.id = id;
     }
     /**
-     * @param nilai_input the nilai_input to set
+     * @return the id_kategori_nilai_materi
      */
-    public void setNilai_input(Float nilai_input) {
-        this.nilai_input = nilai_input;
+    public Long getId_kategori_nilai_materi() {
+        return id_kategori_nilai_materi;
+    }
+    /**
+     * @param id_kategori_nilai_materi the id_kategori_nilai_materi to set
+     */
+    public void setId_kategori_nilai_materi(Long id_kategori_nilai_materi) {
+        this.id_kategori_nilai_materi = id_kategori_nilai_materi;
+    }
+    /**
+     * @return the id_kelas_peserta
+     */
+    public Long getId_kelas_peserta() {
+        return id_kelas_peserta;
+    }
+    /**
+     * @param id_kelas_peserta the id_kelas_peserta to set
+     */
+    public void setId_kelas_peserta(Long id_kelas_peserta) {
+        this.id_kelas_peserta = id_kelas_peserta;
+    }
+    /**
+     * @return the kategoriNilaiMateri
+     */
+    public KategoriNilaiMateri getKategoriNilaiMateri() {
+        return kategoriNilaiMateri;
+    }
+    /**
+     * @param kategoriNilaiMateri the kategoriNilaiMateri to set
+     */
+    public void setKategoriNilaiMateri(KategoriNilaiMateri kategoriNilaiMateri) {
+        this.kategoriNilaiMateri = kategoriNilaiMateri;
+    }
+    /**
+     * @return the kelasPeserta
+     */
+    public KelasPeserta getKelasPeserta() {
+        return kelasPeserta;
+    }
+    /**
+     * @param kelasPeserta the kelasPeserta to set
+     */
+    public void setKelasPeserta(KelasPeserta kelasPeserta) {
+        this.kelasPeserta = kelasPeserta;
     }
     /**
      * @return the nilai_hitung
@@ -151,7 +126,19 @@ public class NilaiSiswa {
     public void setNilai_hitung(Float nilai_hitung) {
         this.nilai_hitung = nilai_hitung;
     }
-
+    /**
+     * @return the nilai_input
+     */
+    public Float getNilai_input() {
+        return nilai_input;
+    }
+    /**
+     * @param nilai_input the nilai_input to set
+     */
+    public void setNilai_input(Float nilai_input) {
+        this.nilai_input = nilai_input;
+    }
+    
     /**
      * @return the created_by
      */
@@ -164,7 +151,7 @@ public class NilaiSiswa {
     public void setCreated_by(Long created_by) {
         this.created_by = created_by;
     }
-    
+
     /**
      * @return the created_date
      */
@@ -202,6 +189,37 @@ public class NilaiSiswa {
      */
     public void setUpdated_date(Date updated_date) {
         this.updated_date = updated_date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NilaiSiswa)) return false;
+        NilaiSiswa that = (NilaiSiswa) o;
+        return Objects.equals(
+            kelasPeserta.getId(), that.kelasPeserta.getId()) &&
+            Objects.equals(kategoriNilaiMateri.getId(), that.kategoriNilaiMateri.getId()) &&
+            Objects.equals(nilai_input, that.nilai_input) &&
+            Objects.equals(nilai_hitung, that.nilai_hitung) &&
+            Objects.equals(created_by, that.created_by) &&
+            Objects.equals(created_date, that.created_date) &&
+            Objects.equals(updated_by, that.updated_by) &&
+            Objects.equals(updated_date, that.updated_date
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+            kelasPeserta.getId(), 
+            kategoriNilaiMateri.getId(), 
+            nilai_input,
+            nilai_hitung,
+            created_by,
+            created_date,
+            updated_by,
+            updated_date
+        );
     }
 
 }
