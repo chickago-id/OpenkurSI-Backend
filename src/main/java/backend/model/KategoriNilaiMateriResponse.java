756134206d568c7b220ff9f507627bb25e981ce8
akbarlaz <akbar.lazuardi@yahoo.com>
2019-10-02 01:42:17 +0700

    2019-10-02 create response for KategoriNilaiMateri

---
+++ src/main/java/backend/model/KategoriNilaiMateriResponse.java
@@ -0,0 +1,40 @@
+package backend.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Author : akbar.lazuardi@yahoo.com | akbarlaz@github.com
+ */
+
+public class KategoriNilaiMateriResponse {
+    public String status;
+    public String message;
+    public ArrayList<KategoriNilaiMateri> data;
+
+    public KategoriNilaiMateriResponse(String status, String message, List<KategoriNilaiMateri> KategoriNilaiMateri) {
+        this.status = status;
+        this.message = message;
+        this.data = new ArrayList<KategoriNilaiMateri>();
+
+        for(KategoriNilaiMateri item:KategoriNilaiMateri) {
+            this.data.add(item);
+        }
+    }
+
+    public KategoriNilaiMateriResponse(String status, String message, KategoriNilaiMateri KategoriNilaiMateri) {
+        this.status = status;
+        this.message = message;
+
+        this.data = new ArrayList<KategoriNilaiMateri>();
+
+        if(KategoriNilaiMateri != null) {
+            this.data.add(KategoriNilaiMateri);   
+        }
+    }
+
+    public KategoriNilaiMateriResponse(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
