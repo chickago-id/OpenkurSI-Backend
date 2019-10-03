ee51d67130ab2fe89d5e3b19c565749173f23fc7
ariesmo <abiesah@gmail.com>
2019-10-03 12:18:38 +0700

    2019-10-03 move to model directory

---
+++ src/main/java/backend/model/UserProfile.java
@@ -0,0 +1,82 @@
+package backend.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Entity
+@Table(name = "user_profile")
+public class UserProfile {
+
+  // @ManyToOne(optional = false)
+  // @JoinColumn(name = "id", referencedColumnName = "id_user", insertable =
+  // false, updatable = false)
+  // private User user;
+
+  // @OneToOne(cascade = CascadeType.ALL)
+  // @OneToOne(optional = false)
+  // @JoinColumn(name = "id", referencedColumnName = "id_user_profile", insertable
+  // = false, updatable = false)
+  // @JoinTable(name = "kelas_peserta", joinColumns = @JoinColumn(name = "id"),
+  // inverseJoinColumns = @JoinColumn(name = "id_user_profile"))
+  // private KelasPeserta kelasPeserta;
+
+  // @OneToMany(cascade=CascadeType.ALL)
+  // @JoinTable(name="propertysub",joinColumns=@JoinColumn(name="propertyid"),
+  // inverseJoinColumns=@JoinColumn(name="propertysubpk"))
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id_user_profile;
+
+  private Long id_kelas;
+  private Long id_user;
+
+  // public KelasPeserta getKelasPeserta() {
+  // return kelasPeserta;
+  // }
+
+  public Long getId_user_profile() {
+    return id_user_profile;
+  }
+
+  public void setId_user_profile(Long id_user_profile) {
+    this.id_user_profile = id_user_profile;
+  }
+
+  public Long getId_kelas() {
+    return id_kelas;
+  }
+
+  public void setId_kelas(Long id_kelas) {
+    this.id_kelas = id_kelas;
+  }
+
+  public Long getId_user() {
+    return id_user;
+  }
+
+  public void setId_user(Long id_user) {
+    this.id_user = id_user;
+  }
+
+}
---
+++ src/main/java/backend/model/UserProfileResponse.java
@@ -0,0 +1,37 @@
+package backend.model;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class UserProfileResponse {
+    public String status;
+    public String message;
+    public ArrayList<UserProfile> data;
+
+    public UserProfileResponse(String status, String message, List<UserProfile> userProfile) {
+        this.status = status;
+        this.message = message;
+        this.data = new ArrayList<UserProfile>();
+
+        for (UserProfile item : userProfile) {
+            this.data.add(item);
+        }
+    }
+
+    public UserProfileResponse(String status, String message, UserProfile userProfile) {
+        this.status = status;
+        this.message = message;
+
+        this.data = new ArrayList<UserProfile>();
+
+        if (userProfile != null) {
+            this.data.add(userProfile);
+        }
+    }
+
+    public UserProfileResponse(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
