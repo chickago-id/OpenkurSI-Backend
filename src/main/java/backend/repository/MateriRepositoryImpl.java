e63b3c746c95581c2ea275b6ba254a958c2978e9
Supi Core <supi.core@gmail.com>
2019-10-07 22:17:10 +0700

    2019-10-07 | create file MateriRepositoryImpl

---
+++ src/main/java/backend/repository/MateriRepositoryImpl.java
@@ -0,0 +1,60 @@
+package backend.repository;
+
+import io.micronaut.configuration.hibernate.jpa.scope.CurrentSession;
+import io.micronaut.spring.tx.annotation.Transactional;
+
+import javax.inject.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import backend.model.Materi;
+
+@Singleton
+public class MateriRepositoryImpl implements MateriRepository{
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public MateriRepositoryImpl(@CurrentSession EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public Materi save(Materi materi) {
+        entityManager.persist(materi);
+        return materi;
+    }
+
+    @Override
+    @Transactional
+    public Materi update(Integer id, Materi materi) {
+        entityManager.merge(materi);
+        return materi;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Materi> findAll() {
+        String qlString = "SELECT m FROM Materi m";
+        TypedQuery<Materi> query = entityManager.createQuery(qlString, Materi.class);
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Materi findById(@NotNull Integer id) {
+        return entityManager.find(Materi.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(@NotNull Integer id) {
+        Materi materi = findById(id);
+
+        if(materi != null) {
+            entityManager.remove(materi);
+        }
+    }
+}
