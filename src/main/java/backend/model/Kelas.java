7d9af7bc4a0c42d8be92fc2b262a7231ac0f0812
ariesmo <abiesah@gmail.com>
2019-10-08 15:05:22 +0700

    2019-10-08 update project

--- src/main/java/backend/model/Kelas.java
+++ src/main/java/backend/model/Kelas.java
@@ -1,12 +1,16 @@
 package backend.model;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.GenerationType;
-import javax.persistence.Table; 
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.OneToOne;
 import javax.persistence.ManyToOne;
 
@@ -15,117 +19,167 @@ import javax.persistence.JoinColumn;
 import java.util.List;
 import java.util.Optional;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Entity
-@Table(name="kelas")
+@Table(name = "kelas")
 public class Kelas {
 
-  @ManyToOne(optional=false)
-  @JoinColumn(name = "id_materi", referencedColumnName="id", insertable = false, updatable = false)
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "id_materi", referencedColumnName = "id", insertable = false, updatable = false)
   private Materi materi;
-  
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-   
-    
-    @NotNull(message = "Kode Kelas harus di isi.")
-    private String kode_kelas;
-    private String jenis_kelas;
-    private String jam_pilihan;
-    private String tanggal_mulai;
-    private String target_peserta;
-    private String jumlah_pertemuan;
-    private String biaya;
-    private String status;
-    private String id_materi;
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "kode_kelas", referencedColumnName = "kode_kelas", insertable = false, updatable = false)
+  private MasterKelas masterKelas;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    public String getKode_kelas() {
-        return kode_kelas;
-    }
+  @NotNull(message = "Kode Kelas harus di isi.")
+  @Column(name = "kode_kelas")
+  private String kode_kelas;
 
-    public void setKode_kelas(String kode_kelas) {
-        this.kode_kelas = kode_kelas;
-    }
+  @Column(name = "jenis_kelas")
+  private String jenis_kelas;
 
-    public String getJenis_kelas() {
-        return jenis_kelas;
-    }
+  @Column(name = "jam_pilihan")
+  private String jam_pilihan;
 
-    public void setJenis_kelas(String jenis_kelas) {
-        this.jenis_kelas = jenis_kelas;
-    }
+  @Column(name = "jam_mulai")
+  private String jam_mulai;
 
+  @Column(name = "jam_selesai")
+  private String jam_selesai;
 
-    public String getJam_pilihan() {
-      return jam_pilihan;
+  @Column(name = "tanggal_mulai", nullable = false, updatable = true)
+  @CreationTimestamp
+  private Date tanggal_mulai;
+
+  @Column(name = "target_peserta")
+  private Integer target_peserta;
+
+  @Column(name = "jumlah_pertemuan")
+  private String jumlah_pertemuan;
+
+  @Column(name = "biaya")
+  private String biaya;
+
+  @Column(name = "status")
+  private String status;
+
+  @Column(name = "id_materi")
+  private Long id_materi;
+
+  public String getKode_kelas() {
+    return kode_kelas;
+  }
+
+  public void setKode_kelas(String kode_kelas) {
+    this.kode_kelas = kode_kelas;
+  }
+
+  public String getJenis_kelas() {
+    return jenis_kelas;
+  }
+
+  public void setJenis_kelas(String jenis_kelas) {
+    this.jenis_kelas = jenis_kelas;
+  }
+
+  public String getJam_pilihan() {
+    return jam_pilihan;
   }
 
   public void setjam_pilihan(String jam_pilihan) {
-      this.jam_pilihan = jam_pilihan;
+    this.jam_pilihan = jam_pilihan;
   }
 
-
-  public String getTanggal_mulai() {
+  public Date getTanggal_mulai() {
     return tanggal_mulai;
-}
+  }
 
-public void setTanggal_mulai(String tanggal_mulai) {
+  public void setTanggal_mulai(Date tanggal_mulai) {
     this.tanggal_mulai = tanggal_mulai;
-}
+  }
 
-public String getTarget_peserta() {
-  return target_peserta;
-}
-
-public void setTarget_peserta(String target_peserta) {
-  this.target_peserta = target_peserta;
-}
-
-public String getJumlah_pertemuan() {
+  public String getJumlah_pertemuan() {
     return jumlah_pertemuan;
   }
-  
+
   public void setJumlah_pertemuan(String jumlah_pertemuan) {
     this.jumlah_pertemuan = jumlah_pertemuan;
   }
-  
-public String getBiaya() {
+
+  public String getBiaya() {
     return biaya;
   }
-  
+
   public void setBiaya(String biaya) {
     this.biaya = biaya;
   }
 
-public String getStatus() {
-  return status;
-}
+  public String getStatus() {
+    return status;
+  }
 
-public void setStatus(String status) {
-  this.status = status;
-}
+  public void setStatus(String status) {
+    this.status = status;
+  }
 
-public String getId_materi() {
-  return id_materi;
-}
+  public Long getId() {
+    return id;
+  }
 
-public void setId_materi(String id_materi) {
-  this.id_materi = id_materi;
-}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-  
-public Materi getMateri() {
+  public Materi getMateri() {
     return materi;
+  }
+
+  public MasterKelas getMasterKelas() {
+    return masterKelas;
+  }
+
+  public String getJam_mulai() {
+    return jam_mulai;
+  }
+
+  public void setJam_mulai(String jam_mulai) {
+    this.jam_mulai = jam_mulai;
+  }
+
+  public String getJam_selesai() {
+    return jam_selesai;
+  }
+
+  public void setJam_selesai(String jam_selesai) {
+    this.jam_selesai = jam_selesai;
+  }
+
+  public Integer getTarget_peserta() {
+    return target_peserta;
+  }
+
+  public void setTarget_peserta(Integer target_peserta) {
+    this.target_peserta = target_peserta;
+  }
+
+  /**
+   * @param masterKelas the masterKelas to set
+   */
+  public void setMasterKelas(MasterKelas masterKelas) {
+    this.masterKelas = masterKelas;
+  }
+
+  public Long getId_materi() {
+    return id_materi;
+  }
+
+  public void setId_materi(Long id_materi) {
+    this.id_materi = id_materi;
   }
 }
