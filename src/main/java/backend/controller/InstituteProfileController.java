948524518579f1b8eb5cb7c8ef7dc1a768275fd2
akbarlaz <akbar.lazuardi@yahoo.com>
2019-10-15 18:56:00 +0700

    2019-10-15 add put request mapping and authentication

--- src/main/java/backend/controller/InstituteProfileController.java
+++ src/main/java/backend/controller/InstituteProfileController.java
@@ -3,14 +3,12 @@ package backend.controller;
 import backend.repository.InstituteProfileRepository;
 import backend.model.InstituteProfile;
 import backend.model.InstituteProfileResponse;
-import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Consumes;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.annotation.Put;
 import io.micronaut.http.multipart.CompletedFileUpload;
 import io.micronaut.http.annotation.Delete;
@@ -21,7 +19,6 @@ import javax.annotation.Nullable;
 
 import com.google.gson.Gson;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -29,6 +26,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Author : akbar.lazuardi@yahoo.com | akbarlaz@github.com
@@ -60,56 +58,121 @@ public class InstituteProfileController {
         String kode_institusi,
         String negara,
         String provinsi,
-        String kota
+        String kota,
+        Authentication auth
         ) {
+            try {
+                if (auth == null) {
+                    InstituteProfileResponse response = new InstituteProfileResponse(
+                        "ERROR", 
+                        "POST DATA FAILED NOT SIGNED IN"
+                    );
+                    return new Gson().toJson(response);
+                } else {
+                    Object data = auth.getAttributes().get("roles");
+                    String roles = data.toString();
+                    if (roles.equals("[\"Admin\"]")) {
+                        try {
+                            Date date = new Date();
+                        String fileName = file.getFilename() + date + ".png";
+                        Path filePath = Files.createTempFile(Paths.get(DIR_PATH), 
+                            file.getFilename(), date + ".png" );
+                        InputStream input = file.getInputStream(); 
+                        Files.copy(input, filePath, StandardCopyOption.REPLACE_EXISTING);
+                        InstituteProfile objIP = new InstituteProfile();
+                        objIP.setNama_institusi(nama_institusi); 
+                        objIP.setAlamat(alamat); 
+                        objIP.setKode_pos(kode_pos);
+                        objIP.setNo_telepon(no_telepon); 
+                        objIP.setWebsite(website); 
+                        objIP.setEmail(email);
+                        objIP.setNpwp(npwp); 
+                        objIP.setKode_institusi(kode_institusi); 
+                        objIP.setNegara(negara);
+                        objIP.setProvinsi(provinsi); 
+                        objIP.setKota(kota); 
+                        objIP.setLogo(fileName);
+                        InstituteProfile result = repoIP.save(objIP);
+                        InstituteProfileResponse response = new InstituteProfileResponse(
+                            "OK", 
+                            "POST DATA SUCCESS",
+                            result
+                        );
+                        return new Gson().toJson(response);    
+                        } catch (IOException e) {
+                            String message = e.getMessage();
+                            InstituteProfileResponse response = new InstituteProfileResponse(
+                                "ERROR", 
+                                message
+                            );
+                            return new Gson().toJson(response);
+                        }
+                    } else {
+                        InstituteProfileResponse response = new InstituteProfileResponse(
+                            "ERROR", 
+                            "POST DATA FAILED NOT Admin"
+                        );
+                        return new Gson().toJson(response);
+                    }
+                }
+            } catch (Exception e) {
+                String msg = e.getMessage();
+                InstituteProfileResponse response = new InstituteProfileResponse(
+                    "ERROR", 
+                    msg
+                );
+                return new Gson().toJson(response);
+            }
+        }
+    
+    @Get("/")
+    @Secured("isAnonymous()")
+    public String index(Authentication auth) {
         try {
-            Date date = new Date();
-            String fileName = file.getFilename() + date + ".png";
-            Path filePath = Files.createTempFile(Paths.get(DIR_PATH), file.getFilename(), date + ".png" );
-            InputStream input = file.getInputStream(); 
-            Files.copy(input, filePath, StandardCopyOption.REPLACE_EXISTING);
-            InstituteProfile objIP = new InstituteProfile();
-            objIP.setNama_institusi(nama_institusi); objIP.setAlamat(alamat); objIP.setKode_pos(kode_pos);
-            objIP.setNo_telepon(no_telepon); objIP.setWebsite(website); objIP.setEmail(email);
-            objIP.setNpwp(npwp); objIP.setKode_institusi(kode_institusi); objIP.setNegara(negara);
-            objIP.setProvinsi(provinsi); objIP.setKota(kota); objIP.setLogo(fileName);
-            InstituteProfile result = repoIP.save(objIP);
+            if (auth == null) {
+                InstituteProfileResponse response = new InstituteProfileResponse(
+                    "ERROR",
+                    "GET DATA FAILED NOT SIGNED IN"
+                );
+                return new Gson().toJson(response);
+            } else {
+                Object data = auth.getAttributes().get("roles");
+                String roles = data.toString();
+                if (roles.equals("[\"Admin\"]")) {
+                    List<InstituteProfile> result = repoIP.findAll();
+                    if (result != null) {
+                        InstituteProfileResponse response = new InstituteProfileResponse(
+                            "OK", 
+                            "GET DATA SUCCESS",
+                            result
+                        );
+                        return new Gson().toJson(response);
+                    } else {
+                        InstituteProfileResponse response = new InstituteProfileResponse(
+                            "ERROR", 
+                            "GET DATA FAILED NOT FOUND",
+                            result
+                        );
+                        return new Gson().toJson(response);
+                    }
+                } else {
+                    InstituteProfileResponse response = new InstituteProfileResponse(
+                        "ERROR", 
+                        "GET DATA FAILED NOT Admin"
+                    );
+                    return new Gson().toJson(response);
+                }
+            }
+        } catch (Exception e) {
+            String msg = e.getMessage();
             InstituteProfileResponse response = new InstituteProfileResponse(
-                "OK", 
-                "POST DATA SUCCESS",
-                result
-            );
-            return new Gson().toJson(response);
-        } catch (IOException e) {
-            String message = e.getMessage();
-            InstituteProfileResponse response = new InstituteProfileResponse(
-                "ERROR", 
-                message
+                "ERROR",
+                msg
             );
             return new Gson().toJson(response);
         }
     }
-    
-    @Post("/{id}/logo")
-    @Secured("isAnonymous()")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces("application/json")
-    public HttpResponse<String> uploadCompleted(Integer id, CompletedFileUpload file) { 
-        InstituteProfile result = repoIP.findById(id);
-        if (result != null) {
-            try {
-                File tempFile = File.createTempFile(file.getFilename(), "temp"); 
-                Path path = Paths.get(tempFile.getAbsolutePath());
-                Files.write(path, file.getBytes()); 
-                return HttpResponse.ok("Uploaded");
-            } catch (IOException exception) {
-                return HttpResponse.badRequest("Upload Failed");
-            }
-        } else {
-            return HttpResponse.notFound("Resource not found");
-        }    
-    }
-    
+
     @Get("/{id}")
     @Secured("isAnonymous()")
     public String show(Integer id, @Nullable Authentication auth) {
@@ -158,59 +221,94 @@ public class InstituteProfileController {
         }
     }
 
-    @Put("/{id}") 
+    @Put("/{id}")
     @Secured("isAnonymous()")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     public String update(
+        CompletedFileUpload file,
         Integer id, 
-        @Body InstituteProfile objIP, 
-        @Nullable Authentication auth) {             
-        try {
-            if(auth == null) {
-                InstituteProfileResponse response = new InstituteProfileResponse(
-                    "ERROR", 
-                    "PUT DATA FAILED NOT SIGNED IN"
-                );
-                return new Gson().toJson(response);
-            } else {
-                Object data = auth.getAttributes().get("roles");
-                String roles = data.toString();
-                if(roles.equals("[\"Admin\"]")) {
-                    InstituteProfile result = repoIP.update(id, objIP);
-                    if(result != null) {
-                        InstituteProfileResponse response = new InstituteProfileResponse(
-                            "OK", 
-                            "PUT DATA SUCCESS", 
-                            result
-                        );
-                        return new Gson().toJson(response);
+        String nama_institusi,
+        String alamat,
+        String kode_pos,
+        String no_telepon,
+        String website,
+        String email,
+        String npwp,
+        String kode_institusi,
+        String negara,
+        String provinsi,
+        String kota,
+        Authentication auth
+    ) 
+        {
+            try {
+                if (auth == null) {
+                    InstituteProfileResponse response = new InstituteProfileResponse(
+                        "ERROR", 
+                        "PUT DATA FAILED NOT SIGNED IN"
+                    );
+                    return new Gson().toJson(response);
+                } else {
+                    Object data = auth.getAttributes().get("roles");
+                    String roles = data.toString();
+                    if (roles.equals("[\"Admin\"]")) {
+                        try {
+                            Date date = new Date();
+                            String fileName = file.getFilename() + date + ".png";
+                            Path filePath = Files.createTempFile(Paths.get(DIR_PATH), 
+                                file.getFilename(), date + ".png" );
+                            InputStream input = file.getInputStream(); 
+                            Files.copy(input, filePath, StandardCopyOption.REPLACE_EXISTING);
+                            InstituteProfile objIP = new InstituteProfile();
+                            objIP.setId(id);
+                            objIP.setNama_institusi(nama_institusi); 
+                            objIP.setAlamat(alamat); 
+                            objIP.setKode_pos(kode_pos);
+                            objIP.setNo_telepon(no_telepon); 
+                            objIP.setWebsite(website); 
+                            objIP.setEmail(email);
+                            objIP.setNpwp(npwp); 
+                            objIP.setKode_institusi(kode_institusi); 
+                            objIP.setNegara(negara);
+                            objIP.setProvinsi(provinsi); 
+                            objIP.setKota(kota); 
+                            objIP.setLogo(fileName);
+                            InstituteProfile result = repoIP.update(id, objIP);
+                            InstituteProfileResponse response = new InstituteProfileResponse(
+                                "OK", 
+                                "PUT DATA SUCCESS",
+                                result
+                            );
+                            return new Gson().toJson(response);    
+                        } catch (IOException e) {
+                            String message = e.getMessage();
+                            InstituteProfileResponse response = new InstituteProfileResponse(
+                                "ERROR", 
+                                message
+                            );
+                            return new Gson().toJson(response);
+                        }
                     } else {
                         InstituteProfileResponse response = new InstituteProfileResponse(
                             "ERROR", 
-                            "PUT DATA FAILED DATA NOT FOUND"
+                            "PUT DATA FAILED NOT Admin"
                         );
                         return new Gson().toJson(response);
                     }
-                } else {
-                    InstituteProfileResponse response = new InstituteProfileResponse(
-                        "ERROR", 
-                        "PUT DATA FAILED NOT Admin"
-                    );
-                    return new Gson().toJson(response);
                 }
+            } catch (Exception e) {
+                String msg = e.getMessage();
+                InstituteProfileResponse response = new InstituteProfileResponse(
+                    "ERROR", 
+                    msg
+                );
+                return new Gson().toJson(response);
             }
-        } catch (Exception e) {
-            String msg = e.getMessage();
-            InstituteProfileResponse response = new InstituteProfileResponse(
-                "ERROR",
-                msg
-            );
-            return new Gson().toJson(response);
         }
-    }
 
     @Delete("/{id}")
     @Secured("isAnonymous()")
-    public String delete(
+    public String delete(@PathVariable(name = "id")
         Integer id, 
         @Nullable Authentication auth) {
         try {
