ecfa2e191c9a306a624dccaaf2d8a738dae27a0f
Supi Core <supi.core@gmail.com>
2019-10-31 21:23:51 +0700

    2019-10-31 | change & add fields relatio kelasRepo

--- src/main/java/backend/repository/KelasRepository.java
+++ src/main/java/backend/repository/KelasRepository.java
@@ -5,11 +5,22 @@ import backend.model.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+/**
+ * Author : supi.core@gmail.com | github.com/sup1core  
+ */
+
 public interface KelasRepository {
    
-    Kelas save(@NotNull Kelas kelas);
-    Kelas update(@NotNull Kelas kelas);
+    Kelas save(Kelas kelas);
+    Kelas update(Integer id, Kelas kelas);
+
     List<Kelas> findAll();
+    List<Kelas> enRoll();
+
+    KelasType cariId(Integer id);
+    MasterKelas findById(Integer id);
+    Batch cariBatchid(Long id_batch);
+
     Kelas findById(@NotNull Long id);
     void deleteById(@NotNull Long id);
 }
--- src/main/java/backend/repository/KelasRepositoryImpl.java
+++ src/main/java/backend/repository/KelasRepositoryImpl.java
@@ -9,10 +9,14 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+
+import backend.model.Batch;
 import backend.model.Kelas;
+import backend.model.KelasType;
+import backend.model.MasterKelas;
 
 @Singleton
-public class KelasRepositoryImpl implements KelasRepository{
+public class KelasRepositoryImpl implements KelasRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -29,7 +33,7 @@ public class KelasRepositoryImpl implements KelasRepository{
 
     @Override
     @Transactional
-    public Kelas update(@NotNull Kelas kelas) {
+    public Kelas update(Integer id, @NotNull Kelas kelas) {
         return entityManager.merge(kelas);
     }
 
@@ -52,8 +56,34 @@ public class KelasRepositoryImpl implements KelasRepository{
     public void deleteById(@NotNull Long id) {
         Kelas kelas = findById(id);
 
-        if(kelas != null) {
+        if (kelas != null) {
             entityManager.remove(kelas);
         }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public MasterKelas findById(Integer id) {
+        return entityManager.find(MasterKelas.class, id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public KelasType cariId(Integer id) {
+        return entityManager.find(KelasType.class, id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Batch cariBatchid(Long id_batch) {
+        return entityManager.find(Batch.class, id_batch);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Kelas> enRoll() {
+        String qlString = "SELECT m FROM Kelas m where status=1";
+        TypedQuery<Kelas> query = entityManager.createQuery(qlString, Kelas.class);
+        return query.getResultList();
     }
 }
